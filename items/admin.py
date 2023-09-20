b2129de728701ac4e5804af16f3902674015b7d5
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-09-20 15:11:46 +0300

    Create django admin for Item model

--- items/admin.py
+++ items/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from items.models import Item
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    search_fields = ("name", "description")
+    ordering = ("name",)
+    readonly_fields = ("stripe_price_id",)
