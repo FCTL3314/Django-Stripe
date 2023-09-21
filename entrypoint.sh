f4c0fd9afc71722d283edcae1324402c8a2ff185
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-09-21 13:30:05 +0300

    fix migration command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 poetry run python manage.py makemigrations
-poetry run python manage.py migratedocker images -a
+poetry run python manage.py migrate
 poetry run gunicorn core.wsgi:application --bind 0.0.0.0:8000
